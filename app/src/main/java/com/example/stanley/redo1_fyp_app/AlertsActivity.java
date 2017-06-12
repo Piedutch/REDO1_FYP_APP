07d2a87852c3df738d70622d8294bd54c8feb9be
Piedutch <zae112@hotmail.com>
2017-06-12 14:45:31 +0800

    JOOOOn

--- app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity.java
+++ app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity.java
@@ -12,6 +12,5 @@ public class AlertsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts);
-        //knock knock who's there
     }
 }
