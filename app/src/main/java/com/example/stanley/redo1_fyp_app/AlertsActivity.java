889b136c0ce247ec0e3fee2cbede8f9159f6f54f
Piedutch <zae112@hotmail.com>
2017-06-30 14:31:45 +0800

    Commit of Jon + Stan Work
    Worked on Stanley' computer, placed functional search bar, but cannot
    filter results yet

--- app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity.java
+++ app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity.java
@@ -1,11 +1,15 @@
 package com.example.stanley.redo1_fyp_app;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -13,6 +17,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
@@ -28,16 +33,39 @@ import java.util.List;
  */
 
 public class AlertsActivity extends Activity {
+
+    private MenuItem searchMenuItem;
+    private SearchView searchView;
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts);
-        ListView list = (ListView) findViewById(R.id.listView);
+        ListView list = (ListView) findViewById(R.id.listView_alerts);
         list.setAdapter(new VivzAdapter(this));
 /*        String[] foods = {"Nasi Lemak","Chicken Rice","Mango","Pineapple"};
         ListAdapter buckysAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
         ListView buckysListView = (ListView) findViewById(R.id.listView);
         buckysListView.setAdapter(buckysAdapter);*/
+        setActionBar();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchMenuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+
+        return true;
+    }
+    private void setActionBar(){
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle("Alerts");
     }
 }
 class SingleRow{
@@ -107,11 +135,11 @@ class VivzAdapter extends BaseAdapter{
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row = inflater.inflate(R.layout.single_row,viewGroup,false);
-        ImageView image = (ImageView) row.findViewById(R.id.imageView);
-        TextView title = (TextView) row.findViewById(R.id.textviewtitle); //Alert Title
+        ImageView image = (ImageView) row.findViewById(R.id.imageView_alerts);
+        TextView title = (TextView) row.findViewById(R.id.textviewtitle_alerts); //Alert Title
       //  TextView timestamp = (TextView) row.findViewById(R.id.timestamp); // Find out how to retrieve current time
-        TextClock timestamp = (TextClock) row.findViewById(R.id.timestamp);
-        TextView description = (TextView) row.findViewById(R.id.description);
+        TextClock timestamp = (TextClock) row.findViewById(R.id.timestamp_alerts);
+        TextView description = (TextView) row.findViewById(R.id.description_alerts);
 
         SingleRow temp = list.get(i);
 
---
+++ app/src/main/java/com/example/stanley/redo1_fyp_app/PicNDescActivity.java
@@ -0,0 +1,8 @@
+package com.example.stanley.redo1_fyp_app;
+
+/**
+ * Created by Stanley on 27/6/2017.
+ */
+
+public class PicNDescActivity {
+}
