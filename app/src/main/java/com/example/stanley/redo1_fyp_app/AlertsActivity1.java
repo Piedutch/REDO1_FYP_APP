b35fcb5a30567525d06c7731331e475f57295e70
wutdequack <jonathanleejuler@gmail.com>
2017-07-14 13:49:46 +0800

    Revert "some changes to unfin work"
    This reverts commit 78efc15b0ced07dac91f8cf1121f8abd8228ac74.
    
    true

--- app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity1.java
+++ app/src/main/java/com/example/stanley/redo1_fyp_app/AlertsActivity1.java
@@ -457,10 +457,8 @@ public class AlertsActivity1 extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        if(item.getItemId() == R.id.archive_button){
-            Intent myIntent = new Intent(getApplicationContext(),ArchiveAlerts.class);
-            startActivity(myIntent);
-        }
+        int id = item.getItemId();
+
         //noinspection SimplifiableIfStatement
 /*        if (id == R.id.action_settings) {
             return true;
