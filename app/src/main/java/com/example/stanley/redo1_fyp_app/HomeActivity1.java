0db999152621e3b37f82c69a7703793381e4a379
wutdequack <jonathanleejuler@gmail.com>
2017-07-26 13:57:55 +0800

    changes to timing for demo
    previous was set to real time, threads coouldn't handle and crashed. Changed to something more manageable. Works successfully.

--- app/src/main/java/com/example/stanley/redo1_fyp_app/HomeActivity1.java
+++ app/src/main/java/com/example/stanley/redo1_fyp_app/HomeActivity1.java
@@ -114,7 +114,7 @@ public class HomeActivity1 extends AppCompatActivity
             Log.d(TAG, "Do i even come inside the counter?");
             alarm_default.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), new_refreshTime, pi_default);
 //            alarm_sysdiag.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.INTERVAL_HALF_DAY, pi_sysdiag);
-            alarm_sysdiag.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), AlarmManager.ELAPSED_REALTIME, pi_sysdiag);
+            alarm_sysdiag.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 60000, pi_sysdiag);
             old_refreshTime = new_refreshTime;
             count++;
             updateCounter(count);
